7d62afcd62a3ca8f5f16087a5b85b18eaf3242ea
Jaedson Silva <contacanal798@gmail.com>
2022-07-09 13:24:15 -0300

    adding arduino project pins

---
+++ chess-clock.ino
@@ -0,0 +1,16 @@
+const int ledOnePin = 7;
+const int ledTwoPin = 8;
+const int buzzerPin = 6;
+
+const int buttonOnePin = 2;
+const int buttonTwoPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
