6b1ec4929c1013315ef5a6239db503c961ad87a7
Jaedson Silva <contacanal798@gmail.com>
2022-07-09 15:01:49 -0300

    waiting first player in setup

--- chess-clock.ino
+++ chess-clock.ino
@@ -1,15 +1,13 @@
 const int ledOnePin = 7;
 const int ledTwoPin = 8;
-const int buzzerPin = 6;
+const int buzzerPin = 5;
 
 const int buttonOnePin = 2;
 const int buttonTwoPin = 13;
 
 int playerOneMillis = 0;
 int playerTwoMillis = 0;
-
 int currentPlayer = 0;
-int startedPlayer = 0;
 
 const int gameTimeMillis = 180000;  // 3 minutes
 
@@ -17,17 +15,33 @@ int getTime(int player);
 void changePlayer();
 
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once:]
+  Serial.begin(9600);
+  
   pinMode(ledOnePin, OUTPUT);
   pinMode(ledTwoPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
   pinMode(buttonOnePin, INPUT_PULLUP);
   pinMode(buttonTwoPin, INPUT_PULLUP);
+
+  digitalWrite(ledOnePin, HIGH);
+  digitalWrite(ledTwoPin, HIGH);
+
+  // wait start player
+  while(true) {
+    if(digitalRead(buttonOnePin) == LOW) {
+      currentPlayer = 1;
+      break;
+    } else if(digitalRead(buttonTwoPin) == LOW) {
+      currentPlayer = 2;
+      break;
+    }
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
 
 int getTime(int player) {
@@ -43,11 +57,11 @@ int getTime(int player) {
 }
 
 void changePlayer() {
-  if(startedPlayer == 1){
+  if(currentPlayer == 1){
     digitalWrite(ledTwoPin, HIGH);
     digitalWrite(ledOnePin, LOW);
     currentPlayer = 2;
-  } else if(startedPlayer == 2){
+  } else if(currentPlayer == 2){
     digitalWrite(ledOnePin, HIGH);
     digitalWrite(ledTwoPin, LOW);
     currentPlayer = 1;
