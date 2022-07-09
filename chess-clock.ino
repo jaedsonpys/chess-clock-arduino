fa867f5c117f0744b333d7379939472b56c9b286
Jaedson Silva <contacanal798@gmail.com>
2022-07-09 14:15:04 -0300

    adding getTime function to get player time

--- chess-clock.ino
+++ chess-clock.ino
@@ -5,12 +5,31 @@ const int buzzerPin = 6;
 const int buttonOnePin = 2;
 const int buttonTwoPin = 13;
 
+int playerOneMillis = 0;
+int playerTwoMillis = 0;
+
+const int gameTimeMillis = 180000;  // 3 minutes
+
+int getTime(int player);
+
 void setup() {
   // put your setup code here, to run once:
-  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+int getTime(int player) {
+  int finalTime;
+  
+  if(player == 1){
+    finalTime = (gameTimeMillis - playerOneMillis) / 1000;
+  } else if(player == 2) {
+    finalTime = (gameTimeMillis - playerTwoMillis) / 1000;
+  }
+
+  return finalTime;
 }
